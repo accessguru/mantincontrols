9ea1e25df7b757637046a2f718270bf719a22a23
accessguru_cp <SND\accessguru_cp>
2014-09-08 16:25:25 +0000

    Initial Checkin

---
+++ Trunk/ToastPopup/ToastPopup/NotificationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Foundation.Common.Controls.Wpf.Notification
+{
+    public enum NotificationType : int
+    {
+        Information = 0,
+
+        Warning = 1,
+
+        Error = 2
+    }
+}
