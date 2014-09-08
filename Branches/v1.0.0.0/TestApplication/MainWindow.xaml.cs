df97402ce00e9bb708a287aedd8de8eccf33b818
accessguru_cp <SND\accessguru_cp>
2014-09-08 17:09:55 +0000

    Branched from $/toastspopuphelpballoon/Trunk

---
+++ Branches/v1.0.0.0/TestApplication/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace TestApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new ViewModel();
+        }
+    }
+}
