a17f493d89270c7c862dd415799084d21b5a4025
accessguru_cp <SND\accessguru_cp>
2014-09-15 19:08:14 +0000

    Branched from $/toastspopuphelpballoon/Trunk

---
+++ Branches/v2.2.2.0/TestApplication/ObservableBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DemoApplication
+{
+    [Serializable]
+    public abstract class ObservableBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Property Changed Event
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Get the string name for the property
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        protected string GetPropertyName<T>(Expression<Func<T>> expression)
+        {
+            var memberExpression = (MemberExpression)expression.Body;
+            return memberExpression.Member.Name;
+        }
+
+        /// <summary>
+        /// Notify Property Changed (Shorted method name)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression"></param>
+        protected virtual void Notify<T>(Expression<Func<T>> expression)
+        {
+            string propertyName = this.GetPropertyName(expression);
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        /// <summary>
+        /// Called when [property changed].
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression">The expression.</param>
+        protected virtual void OnPropertyChanged<T>(Expression<Func<T>> expression)
+        {
+            string propertyName = this.GetPropertyName(expression);
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
