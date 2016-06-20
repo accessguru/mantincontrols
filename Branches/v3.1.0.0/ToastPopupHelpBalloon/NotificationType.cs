cb88944d9b14b34ca8e02a6a3c7a26cc8adbb0a9
accessguru_cp <SND\accessguru_cp>
2016-06-20 20:36:15 +0000

    Branched from $/toastspopuphelpballoon/Trunk

---
+++ Branches/v3.1.0.0/ToastPopupHelpBalloon/NotificationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Mantin.Controls.Wpf.Notification
+{
+    public enum NotificationType : int
+    {
+        Information = 0,
+
+        Warning = 1,
+
+        Error = 2
+    }
+}
