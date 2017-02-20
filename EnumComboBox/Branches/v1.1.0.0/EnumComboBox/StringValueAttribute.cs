9621e8b600d30860837e1b7df3c32af719b7c501
accessguru_cp <SND\accessguru_cp>
2017-02-20 14:03:23 +0000

    Branched from $/toastspopuphelpballoon/EnumComboBox/Trunk

---
+++ EnumComboBox/Branches/v1.1.0.0/EnumComboBox/StringValueAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Mantin.Controls.Wpf.EnumComboBox
+{
+    /// <summary>
+    /// Simple attribute class for storing String Values
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All)]
+    public sealed class StringValueAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringValueAttribute"/> class.
+        /// </summary>
+        public StringValueAttribute() 
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringValueAttribute"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public StringValueAttribute(string value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value></value>
+        public string Value { get; private set; }
+    }
+}
