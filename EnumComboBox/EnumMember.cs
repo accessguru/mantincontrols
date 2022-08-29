135f048f570d5b810183297a4e641b1d1382fa8e
Lawrence Mantin <lawrence.mantin@sprbrk.com>
2022-08-29 09:15:30 -0500

    project restructure

---
+++ EnumComboBox/EnumMember.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Mantin.Controls.Wpf.EnumComboBox
+{
+    public class EnumMember<T> 
+        where T : struct, IConvertible
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public T Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the string value.
+        /// </summary>
+        /// <value>
+        /// The string value.
+        /// </value>
+        public string StringValue { get; set; }
+
+        #endregion Properties
+
+        #region Public Methods
+
+        /// <summary>
+        /// Converts to list.
+        /// </summary>
+        /// <returns>
+        /// A List of EnumMembers.
+        /// </returns>
+        /// <exception cref="System.ArgumentException">T must be of type enumeration.</exception>
+        public List<EnumMember<T>> ConvertToList()
+        {
+            Type type = typeof(T);
+
+            if (!type.IsEnum)
+            {
+                throw new ArgumentException("T must be of type enumeration.");
+            }
+
+            var members = new List<EnumMember<T>>();
+            MemberInfo[] memberInfos = type.GetMembers(BindingFlags.Public | BindingFlags.Static);
+
+            foreach (var memberInfo in memberInfos)
+            {
+                object attr = memberInfo.GetCustomAttributes(true).SingleOrDefault(a => a is StringValueAttribute);
+
+                var enumMember = new EnumMember<T>
+                {
+                    Description = attr != null ? ((StringValueAttribute) attr).Value : memberInfo.Name,
+                    Value = (T) Enum.Parse(type, memberInfo.Name),
+                    StringValue = memberInfo.Name
+                };
+
+                members.Add(enumMember);
+            }
+
+            return members;
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ EnumComboBox/StringValueAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Mantin.Controls.Wpf.EnumComboBox
+{
+    /// <summary>
+    /// Simple attribute class for storing String Values
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All)]
+    public sealed class StringValueAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringValueAttribute"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public StringValueAttribute(string value)
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                this.Value = value;
+                return;
+            }
+
+            throw new ArgumentNullException(nameof(value));
+        }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value></value>
+        public string Value { get; }
+    }
+}
